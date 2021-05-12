529055de2901efc5e8153fd127644331c2a3f31f
Blessing Hirwa <hirwablessing7@gmail.com>
2021-05-12 11:16:10 +0200

    Change colors for functions and some other attributes.

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-import * as path from 'path';
-import { generateTheme, IColorSet } from 'vscode-theme-generator';
+import * as path from "path";
+import { generateTheme, IColorSet } from "vscode-theme-generator";
 
-const themeName = 'Generated';
+const themeName = "Generated";
 const colors = {
-  black: '#111111',
-  white: '#FFFFFF',
-  blue: '#5555FF',
-  red: '#FF5555',
-  green: '#55FF55',
-  yellow: '#FFFF33'
-}
+  black: "#1f2937",
+  white: "#FFFFFF",
+  blue: "#E6DB74",
+  red: "#f92672",
+  green: "#a6e22e",
+  yellow: "#FFFFFF",
+};
 
 const colorSet: IColorSet = {
   base: {
     background: colors.black,
     foreground: colors.white,
-    color1: colors.blue,
-    color2: colors.red,
+    color1: colors.red,
+    color2: colors.blue,
     color3: colors.green,
-    color4: colors.yellow
-  }
+    color4: colors.yellow,
+  },
 };
 
-generateTheme(themeName, colorSet, path.join(__dirname, 'theme.json'));
+generateTheme(themeName, colorSet, path.join(__dirname, "theme.json"));
